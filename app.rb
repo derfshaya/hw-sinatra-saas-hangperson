7818f592f336a2b3d3df9c61837134c0c2f3e19c
Derf Shaya <derfshaya@gmail.com>
2015-11-08 21:18:31 +0000

    Tried to deal with corner cases

--- app.rb
+++ app.rb
@@ -40,19 +40,8 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if (@game.guess(letter))
-      case @game.check_win_or_lose
-      when :win
-        redirect '/win'
-      when :lose
-        redirect '/lose'
-      else
-        redirect '/show'
-      end
-    else
-      flash[:message] = "You have already used that letter"
-      redirect '/show'
-    end
+    flash[:message] = "You have already used that letter" if !@game.guess(letter)
+    redirect '/show'
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -62,7 +51,14 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
