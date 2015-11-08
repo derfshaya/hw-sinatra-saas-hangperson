29ea3b55ef3da74247bb2375beee1470b1e3f3f5
Derf Shaya <derfshaya@gmail.com>
2015-11-08 20:19:38 +0000

    Defined HangpersonGame object

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,46 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || (!HangpersonGame.letter? letter)
+    lower_letter = letter.downcase
+    if @word.include? lower_letter
+      if !@guesses.include? lower_letter
+        @guesses << lower_letter
+      else
+        false
+      end
+    else
+      if !@wrong_guesses.include? lower_letter
+        @wrong_guesses << lower_letter
+      else
+        false
+      end
+    end
+  end
+  
+  def word_with_guesses
+    guessed_word = ""
+    @word.split("").each do |letter|
+      guessed_word << ((@guesses.include? letter) ? letter : "-")
+    end
+    return guessed_word
+  end
+  
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+    return :win if !word_with_guesses.include? "-"
+    return :play
   end
 
   def self.get_random_word
@@ -17,6 +54,11 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+  
+  private
+  def self.letter?(letter)
+    letter =~ /[A-Za-z]/
   end
 
 end
